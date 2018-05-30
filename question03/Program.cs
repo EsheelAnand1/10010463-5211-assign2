a3a0e0643bb97d0e74bffa2593ec1960bf508661
EsheelAnand1 <j4alous1>
2018-05-30 15:28:33 +1200

    creating a stopwatch on line 10 and 11

--- question03/Program.cs
+++ question03/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace question03
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { Stopwatch sw = new Stopwatch();
+            sw.Start();
             string[] fruitsArray = new string[] {"Banana", "Grapefruit", "Tomato", "Oranges"};
 
             //List<string> fruitsArray = new List<string> {"Banana", "Grapefruit", "Tomato", "Oranges"};
